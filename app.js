148ec292a573de755d031acc7988b2f230016982
MoizNadeemLone <moiznadeemlone1@gmail.com>
2025-09-17 06:31:49 +0500

    added database image

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
-    user: 'superuser',
-    pass: 'SuperPassword',
+mongoose.connect(process.env.MONGO_URL, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
